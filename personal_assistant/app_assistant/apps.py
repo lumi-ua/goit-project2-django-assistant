9d53c5f94836efcf105beb49e824d8e074d7ac9a
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2023-12-11 00:24:48 +0200

    add app_assistant

---
+++ personal_assistant/app_assistant/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppAssistantConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_assistant'
--- personal_assistant/personal_assistant/settings.py
+++ personal_assistant/personal_assistant/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_assistant',
+    'app_contacts',
     "noteapp",
+    
     
     
 ]
