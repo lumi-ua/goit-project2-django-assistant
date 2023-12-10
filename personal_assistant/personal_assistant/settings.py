e5e9d7097561552c7efba3b9952a56036271517f
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2023-12-11 00:53:17 +0200

    reverted previous mistaked commit

--- personal_assistant/personal_assistant/settings.py
+++ personal_assistant/personal_assistant/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_assistant',
-    'app_contacts',
     "noteapp",
     
     
