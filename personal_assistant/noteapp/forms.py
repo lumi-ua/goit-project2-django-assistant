c65fd71eb1ad8b8bad56220c84e8e7a048de6928
SerhiiAndreiko <130772110+SerhiiAndreiko@users.noreply.github.com>
2023-12-12 23:27:51 +0100

    add models, views

--- personal_assistant/noteapp/forms.py
+++ personal_assistant/noteapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Note
 
 
-class NoteForms(forms.ModelForm):
+class NoteForm(forms.ModelForm):
     class Meta:
         model = Note
         fields = ('name', 'description', 'done', 'tags',)
