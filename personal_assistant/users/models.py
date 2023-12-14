8818f478ee7b56890d372b334f511135a5b68e69
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-12-14 21:51:53 +0200

    first

---
+++ personal_assistant/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
