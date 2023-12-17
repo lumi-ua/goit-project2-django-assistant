6a4b40739280d7b10616be8ac5678c07691c4763
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-12-17 19:14:15 +0200

    edit

--- personal_assistant/app_contacts/forms.py
+++ personal_assistant/app_contacts/forms.py
@@ -5,43 +5,37 @@ from phonenumber_field.widgets import PhoneNumberPrefixWidget
 from .models import Contact, PhoneNumber, EmailAddress
 
 
-
-
 class ContactForm(ModelForm):
-    fullname = CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Name Lastname', "class": "form-control"}))
-    address = CharField(max_length=255, required=False, widget=forms.TextInput(attrs={'placeholder': 'Сity, Street, House number', "class": "form-control"}))  
-    birthday = DateField(required=False, input_formats=["%d.%m.%Y"], widget=forms.DateInput(attrs={'placeholder': 'DD.MM.YYYY', 'class': 'form-control'}))                        
+    fullname = CharField(max_length=255, widget=forms.TextInput(
+        attrs={'placeholder': 'Name Lastname', "class": "form-control"}))
+    address = CharField(max_length=255, required=False, widget=forms.TextInput(
+        attrs={'placeholder': 'Сity, Street, House number', "class": "form-control"}))
+    birthday = DateField(required=False, input_formats=["%d.%m.%Y"], widget=forms.DateInput(
+        attrs={'placeholder': 'DD.MM.YYYY', 'class': 'form-control'}))
+
     class Meta:
         model = Contact
         fields = ["fullname", "address", "birthday"]
         exclude = ["user"]
 
-    
 
 class PhoneNumberForm(forms.ModelForm):
-        phone_number = PhoneNumberField(
-            widget=PhoneNumberPrefixWidget(attrs={'placeholder': '+380', 'class': 'form-control'})
-        )
-        class Meta:
-            model = PhoneNumber
-            fields = ["phone_number"]
-            exclude = ["contact"]
+    phone_number = PhoneNumberField(
+        widget=PhoneNumberPrefixWidget(
+            attrs={'placeholder': '+380', 'class': 'form-control'})
+    )
+
+    class Meta:
+        model = PhoneNumber
+        fields = ["phone_number"]
+        exclude = ["contact"]
+
 
 class EmailAddressForm(forms.ModelForm):
-        email = EmailField(max_length=100, required=True, widget=forms.EmailInput(attrs={'placeholder': 'example@email.com', 'class': 'form-control'}))
+    email = EmailField(max_length=100, required=True, widget=forms.EmailInput(
+        attrs={'placeholder': 'example@email.com', 'class': 'form-control'}))
 
-        class Meta:
-            model = EmailAddress
-            fields = ["email"]
-            exclude = ["contact"]
-
-
-    
-
-
-    
-
-
-    
-
-
+    class Meta:
+        model = EmailAddress
+        fields = ["email"]
+        exclude = ["contact"]
