31de58e98ae4fb236a9ec5e71bd347286c2e0f38
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2023-12-17 17:55:00 +0200

    added user into views

--- personal_assistant/app_contacts/models.py
+++ personal_assistant/app_contacts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 # Create your models here.
@@ -10,12 +11,12 @@ class Contact(models.Model):
     fullname = models.CharField(max_length=255)
     address = models.CharField(max_length=255, blank=True, null=True)
     birthday = models.DateField(blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     
 
 class EmailAddress(models.Model):
-    email = models.EmailField(unique=True, max_length=20, null=True)
+    email = models.EmailField(max_length=100, null=True)
     contact = models.ForeignKey(
         Contact, on_delete=models.CASCADE, default=None, null=True, related_name='email_addresses'
     )
--- personal_assistant/app_contacts/views.py
+++ personal_assistant/app_contacts/views.py
@@ -22,13 +22,17 @@ def contact(request):
         email_address_form = EmailAddressForm(request.POST)
 
         if contact_form.is_valid() and phone_number_form.is_valid() and email_address_form.is_valid():
-            new_contact = contact_form.save() 
+            new_contact = contact_form.save(commit=False)
+            new_contact.user = request.user
+            new_contact.save()
 
             phone_number = phone_number_form.save(commit=False)
             phone_number.contact = new_contact
             phone_number.save()
 
+            
             email_address = email_address_form.save(commit=False)
+            
             email_address.contact = new_contact
             email_address.save()
 
@@ -49,17 +53,19 @@ def contact(request):
         },
     )
 
+@login_required
 def contacts(request):
-    contacts = Contact.objects.all()
-    return render(request, "app_contacts/all_contacts.html", {"contacts_form": contacts})
+    contacts = Contact.objects.filter(user=request.user)
+    return render(request, "app_contacts/all_contacts.html", {"contacts": contacts})
 
 
+@login_required
 def detail(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
     return render(request, 'app_contacts/detail.html', {'contact': contact})
 
 
-
+@login_required
 def add_phone_number(request, pk):
     contact = Contact.objects.get(pk=pk)
     phone_number_add_url = reverse_lazy('app_contacts:add_phone_number', kwargs={'pk': pk})
@@ -81,7 +87,7 @@ def add_phone_number(request, pk):
     })
 
 
-
+@login_required
 def add_email_address(request, pk):
     contact = Contact.objects.get(pk=pk)
     email_adress_add_url = reverse_lazy('app_contacts:add_email_address', kwargs={'pk': pk})
@@ -101,29 +107,33 @@ def add_email_address(request, pk):
                     'email_adress_add_url': email_adress_add_url
                     })
 
-
+@login_required
 def upcoming_birthdays(request):
     today = date.today()
     days_in_future = int(request.GET.get("days", 7))
 
     contacts = Contact.objects.filter(
         birthday__month__gte=today.month,
-        birthday__day__lte=today.day + days_in_future,        
+        birthday__day__lte=today.day + days_in_future,
+        user=request.user,
     )
+
     if not contacts.exists():
         return render(request, "app_contacts/upcoming_birthdays.html", {"message": "No upcoming birthdays."})
-    
+
     return render(request, "app_contacts/upcoming_birthdays.html", {"contacts": contacts})
 
-    
 
+
+
+@login_required
 def search_contacts(request):
     query = request.GET.get("query", "")
     error_message = ""
-    
 
     try:
-        contacts = Contact.objects.filter(
+        user_contacts = Contact.objects.filter(user=request.user)
+        contacts = user_contacts.filter(
             Q(fullname__icontains=query)
             | Q(phone_numbers__phone_number__icontains=query)
             | Q(email_addresses__email__icontains=query)
@@ -131,19 +141,18 @@ def search_contacts(request):
     except Contact.DoesNotExist:
         contacts = []
         error_message = "Сontact not found"
-        
 
     return render(request, "app_contacts/search_contacts.html", {"contacts": contacts, "error_message": error_message})
 
 
 
-
+@login_required
 def edit_contact(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
     birthday_str = contact.birthday.strftime("%d.%m.%Y") if contact.birthday else ""
 
     if request.method == "POST":
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, instance=contact, user=request.user)
         if form.is_valid():
             form.save()
             messages.success(request, "Контакт успешно отредактирован")
@@ -161,6 +170,7 @@ def edit_contact(request, pk):
     })
 
 
+@login_required
 def delete_contact(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
 
@@ -170,7 +180,7 @@ def delete_contact(request, pk):
         return redirect(to="app_assistant:main")
 
     else:
-        return render(request, "app_contacts/delete_contact.html", {"contact": contact})
+        return render(request, "app_contacts/delete_contact.html", {"contact": contact, "user": request.user})
     
 
 # def delete_email(request, pk):
